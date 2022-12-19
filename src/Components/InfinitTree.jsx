4b16289370ac38c7d39d1c80f224c587551bfff9
Zarm1337 <skyrembo@gmail.com>
2022-12-19 23:56:49 +0300

    Added random dependant items generator, use Xarrow.

--- src/Components/InfinitTree.jsx
+++ src/Components/InfinitTree.jsx
@@ -1,6 +1,8 @@
 import { useState, useCallback } from 'react'
 import { VariableSizeGrid as Grid } from 'react-window';
 import InfiniteLoader from "react-window-infinite-loader";
+import uniqueId from 'lodash/uniqueId'
+import Xarrow from "react-xarrows";
 
 const DEMO_DATA = [
   { items: [1, 2, 3] },
@@ -9,40 +11,70 @@ const DEMO_DATA = [
   { items: [3, 5, 6] },
 ]
 
-// These item sizes are arbitrary.
-// Yours should be based on the content of the item.
-const columnWidths = new Array(1000)
-  .fill(true)
-  .map(() => 75 + Math.round(Math.random() * 50));
-const rowHeights = new Array(1000)
-  .fill(true)
-  .map(() => 25 + Math.round(Math.random() * 50));
+const START_ITEM = {
+  prevItemId: null,
+  id: uniqueId(),
+  isMain: true,
+}
 
+const getPrevLine = (initItem) => {
+  const itemsAmount = Math.floor(Math.random() * 10) || 1
+  const newItems = new Array(itemsAmount).fill(0).map((item, index) => ({
+    prevItemId: initItem.id,
+    id: uniqueId(),
+    isMain: index === 0,
+  }))
+  return newItems
+}
 
+const generateNewItems = (startItem, amount) => {
+  const array = new Array(amount)
+  for (let i = 0; i < amount; i++) {
+    const initItem = i > 0 ? array[i - 1].items[0] : startItem
+    array[i] = { items: [...getPrevLine(initItem)] }
+  }
+  return array
+}
 
 const Example = () => {
-  const [{ items, moreItemsLoading, hasNextPage }, setItems] = useState({
-    items: DEMO_DATA,
+  const [{ data: items, moreItemsLoading, hasNextPage }, setItems] = useState({
+    data: [{ items: [START_ITEM] }].concat(generateNewItems(START_ITEM, 4)),
     moreItemsLoading: false,
     hasNextPage: true
   })
 
   const Cell = ({ columnIndex, rowIndex, style }) => {
     const row = items[rowIndex]
-    return row && row.items[columnIndex] && (
+    const rowItem = row.items[columnIndex]
+    console.log(60 * columnIndex)
+    return row && rowItem && (
       <div style={style} >
-        <div style={{
-          margin: '8px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: 'calc(100% - 8px)',
-          height: 'calc(100% - 8px)',
-          border: '1px solid black',
-          borderRadius: '50%'
-        }}>
-          Item {rowIndex},{columnIndex}
+        <div
+          id={`item-${rowItem.id}`}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 'calc(100% - 8px)',
+            height: 'calc(100% - 8px)',
+            border: '1px solid black',
+            borderRadius: '50%'
+          }}>
+          id: {rowItem.id}<br />
+          prevId: {rowItem.prevItemId}<br />
+          isMain: {rowItem.isMain ? '+' : '-'}<br />
         </div>
+        {rowItem.prevItemId &&
+          <Xarrow
+            start={`item-${rowItem.id}`}
+            end={`item-${rowItem.prevItemId}`}
+            path="straight"
+            startAnchor={'top'}
+            endAnchor={'bottom'}
+            showHead={false}
+            showTail={false}
+          />
+        }
       </div>)
   };
 
@@ -52,11 +84,13 @@ const Example = () => {
       setItems(prev => ({ ...prev, moreItemsLoading: true }))
       setTimeout(() => {
         console.log('timeout')
-        setItems(prev => ({
-          items: [...prev.items].concat(prev.items),
-          moreItemsLoading: false,
-          hasNextPage: true
-        }))
+        setItems(prev => {
+          return ({
+            data: [...prev.data].concat(generateNewItems(prev.data[prev.data.length - 1].items[0], 4)),
+            moreItemsLoading: false,
+            hasNextPage: true
+          })
+        })
       }, 1000)
     },
     [],
@@ -74,10 +108,10 @@ const Example = () => {
         <Grid
           columnCount={Math.max(...DEMO_DATA.map(item => item.items.length))}
           columnWidth={index => 100}
-          height={300}
+          height={1000}
           rowCount={items.length}
           rowHeight={index => 100}
-          width={500}
+          width={1000}
           onItemsRendered={({
             visibleRowStartIndex,
             visibleRowStopIndex,
